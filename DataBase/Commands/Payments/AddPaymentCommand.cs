a400a5a3b2aeb09e96f138a2168021029a18bd36
Dmitry <chiruhind@gmail.com>
2023-03-12 01:26:51 +0500

    Add p 0 when p for somebody

--- DataBase/Commands/Payments/AddPaymentCommand.cs
+++ DataBase/Commands/Payments/AddPaymentCommand.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using DataBase.Models;
 
 namespace DataBase.Commands
@@ -14,6 +15,15 @@ namespace DataBase.Commands
 
         public async Task ExecuteAsync(PaymentDto dto)
         {
+            if (dto.UserToId != null)
+            {
+                var payment = _db.Payments.FirstOrDefault(x => x.UserFromId == dto.UserFromId && x.UserToId == null && x.PayDate == null);
+                if (payment == null)
+                {
+                    _db.Payments.Add(new PaymentDto{Amount = 0, UserFromId = dto.UserFromId});
+                }
+            }
+            
             _db.Payments.Add(dto);
             await _db.SaveChangesAsync();
         }
